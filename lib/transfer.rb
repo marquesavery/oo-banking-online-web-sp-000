2d6fbd644f499969d81596989f4075c994944c22
Marques Avery <averymarques4@gmail.com>
2020-08-27 20:50:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+    # binding.pry
     if @sender.balance < self.amount
       "Transaction rejected. Please check your account balance."
     elsif self.status == "pending"
